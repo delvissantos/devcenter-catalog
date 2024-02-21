9f41de1bd2b042f95342ba9cc940ddde52e4e09e
delvissantos <106829780+delvissantos@users.noreply.github.com>
2024-02-20 23:03:42 -0500

    Update InstallAMA.ps1

--- Tasks/amainstall/InstallAMA.ps1
+++ Tasks/amainstall/InstallAMA.ps1
@@ -3,7 +3,7 @@
 $packagePath = [System.IO.Path]::GetTempFileName() + ".msi"
 Invoke-RestMethod -Uri https://go.microsoft.com/fwlink/?linkid=2192409 -OutFile $packagePath
 $logPath = "C:\DevboxAgent\Temp\Logs\AMALogs\0.log"
+Write-Host "Test change"
 New-Item -Force -Path $logPath
 $ArgumentList=@("/i", $packagePath, "/qn", "/li", $logPath)
 Start-Process msiexec -ArgumentList $ArgumentList -Wait -Verbose
-Write-Host "Process complete"
