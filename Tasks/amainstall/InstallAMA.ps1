7cd6c7b7aaf6dd0dfea660b517687c7374fe6671
delvissantos <106829780+delvissantos@users.noreply.github.com>
2024-02-20 22:57:02 -0500

    Update InstallAMA.ps1

--- Tasks/amainstall/InstallAMA.ps1
+++ Tasks/amainstall/InstallAMA.ps1
@@ -1,8 +1,16 @@
 # Installs the Azure Monitor Agent via client MSI
 
 $packagePath = [System.IO.Path]::GetTempFileName() + ".msi"
-Invoke-RestMethod -Uri https://delvisstorageeast.blob.core.windows.net/ama/AzureMonitorAgentClientSetupDevboxSupport.msi -OutFile $packagePath
-$logPath = "C:\DevboxAgent\Temp\Logs\AMALogs\0.log"
-New-Item -Force -Path $logPath
-$ArgumentList=@("/i", $packagePath, "/qn", "/li", $logPath)
-Start-Process msiexec -ArgumentList $ArgumentList -Wait -Verbose
+  try {
+    Invoke-RestMethod -Uri https://delvisstorageeast.blob.core.windows.net/ama/AzureMonitorAgentClientSetupDevboxSupport.msi -OutFile $packagePath
+    $logPath = "C:\DevboxAgent\Temp\Logs\AMALogs\0.log"
+    New-Item -Force -Path $logPath
+    $ArgumentList=@("/i", $packagePath, "/qn", "/li", $logPath)
+    Start-Process msiexec -ArgumentList $ArgumentList -Wait -Verbose
+} 
+catch {
+        $streamReader = [System.IO.StreamReader]::new($_.Exception.Response.GetResponseStream())
+        $ErrResp = $streamReader.ReadToEnd() | ConvertFrom-Json
+        $streamReader.Close()
+      }
+  $ErrResp
