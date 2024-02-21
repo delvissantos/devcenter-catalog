b13dd095bf98e99ab57a74d423ccb4bc58980301
delvissantos <106829780+delvissantos@users.noreply.github.com>
2024-02-20 23:00:55 -0500

    Update InstallAMA.ps1

--- Tasks/amainstall/InstallAMA.ps1
+++ Tasks/amainstall/InstallAMA.ps1
@@ -1,16 +1,9 @@
 # Installs the Azure Monitor Agent via client MSI
 
 $packagePath = [System.IO.Path]::GetTempFileName() + ".msi"
-  try {
-    Invoke-RestMethod -Uri https://delvisstorageeast.blob.core.windows.net/ama/AzureMonitorAgentClientSetupDevboxSupport.msi -OutFile $packagePath
-    $logPath = "C:\DevboxAgent\Temp\Logs\AMALogs\0.log"
-    New-Item -Force -Path $logPath
-    $ArgumentList=@("/i", $packagePath, "/qn", "/li", $logPath)
-    Start-Process msiexec -ArgumentList $ArgumentList -Wait -Verbose
-} 
-catch {
-        $streamReader = [System.IO.StreamReader]::new($_.Exception.Response.GetResponseStream())
-        $ErrResp = $streamReader.ReadToEnd() | ConvertFrom-Json
-        $streamReader.Close()
-      }
-  $ErrResp
+Invoke-RestMethod -Uri https://go.microsoft.com/fwlink/?linkid=2192409 -OutFile $packagePath
+$logPath = "C:\DevboxAgent\Temp\Logs\AMALogs\0.log"
+New-Item -Force -Path $logPath
+$ArgumentList=@("/i", $packagePath, "/qn", "/li", $logPath)
+Start-Process msiexec -ArgumentList $ArgumentList -Wait -Verbose
+Write-Host "Process complete"
